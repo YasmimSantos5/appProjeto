49d637ee61ce28ae5e3e321c34f3e76f4d6653e9
Yasmim <yasmimgrilo@hotmail.com>
2026-05-19 21:57:14 -0300

    Adicionando funcionalidae do botão boleto

---
+++ app/src/main/java/com/example/projetoapp/Boleto.kt
@@ -0,0 +1,10 @@
+package com.example.projetoapp
+
+import android.R
+
+data class Boleto(
+    val titulo : String,
+    val valor : String,
+    val vencimento : String
+    )
+
--- app/src/main/java/com/example/projetoapp/BoletoActivity.kt
+++ app/src/main/java/com/example/projetoapp/BoletoActivity.kt
@@ -1,33 +1,31 @@
 package com.example.projetoapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import android.support.v4.os.IResultReceiver
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-class PainelActivity : AppCompatActivity() {
-
+class BoletoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_painel)
-
+        setContentView(R.layout.activity_boleto)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        val botaoBoleto = findViewById<Button>(R.id.btnBoleto)
-
-        botaoBoleto.setOnClickListener {
-
-            val intent = Intent(this, BoletoActivity::class.java)
-            startActivity(intent)
-
-        }
+        val boletos = listOf(
+            Boleto("Taxa Condominio Maio", "R$ 1500", "18/05/2026"),
+            Boleto("Taxa Condominio Maio", "R$ 1500", "18/05/2026"),
+            Boleto("Taxa Condominio Maio", "R$ 1500", "18/05/2026"),
+        )
+        val rc = findViewById<RecyclerView>(R.id.RecycleVBiewBoleto)
+        rc.layoutManager = LinearLayoutManager(this)
+        rc.adapter = BoletoAdapter(boletos)
     }
 }
